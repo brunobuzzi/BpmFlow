42dc8a8aeff464484a39daca32ced8d0d7ac1bdd
bruno.buzzi <bruno.brasesco@gmail.com>
2018-08-27 16:23:24 -0300

    fix #582

--- start-timers-loop.sh
+++ start-timers-loop.sh
@@ -2,7 +2,7 @@
 #! /bin/sh
 #set -x
 $GS_HOME/bin/startTopaz devKit_34 -il <<EOF >>MFC.out
-set user DataCurator password swordfish gemstone devKit_33
+set user DataCurator password swordfish gemstone devKit_34
 login
 exec 
    | handler commitThreshold |
