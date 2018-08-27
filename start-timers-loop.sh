4715c3e959d764cdef74e84561be631b325356ad
bruno.buzzi <bruno.brasesco@gmail.com>
2018-08-27 16:15:27 -0300

    fix #582

--- start-timers-loop.sh
+++ start-timers-loop.sh
@@ -1,7 +1,7 @@
 #Requires GS_HOME variable defined
 #! /bin/sh
 #set -x
-$GS_HOME/bin/startTopaz devKit_33 -il <<EOF >>MFC.out
+$GS_HOME/bin/startTopaz devKit_34 -il <<EOF >>MFC.out
 set user DataCurator password swordfish gemstone devKit_33
 login
 exec 
--- stop-zn.sh
+++ stop-zn.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 #set -x
-$GS_HOME/bin/startTopaz devKit_33 -il <<EOF >>MFC.out
-set user DataCurator password swordfish gemstone devKit_33
+$GS_HOME/bin/startTopaz devKit_34 -il <<EOF >>MFC.out
+set user DataCurator password swordfish gemstone devKit_34
 login
 exec 
 System beginTransaction.
