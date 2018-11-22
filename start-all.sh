c5736131a3d1b1ff613c71c94d384e578c48978d
bruno.buzzi <bruno.brasesco@gmail.com>
2018-11-22 09:57:37 -0300

    fix #434

--- start-all.sh
+++ start-all.sh
@@ -5,16 +5,26 @@ $GS_HOME/bin/startTopaz devKit_34 -il <<EOF >>MFC.out
 set user DataCurator password swordfish gemstone devKit_34
 login
 exec 
-   | handler commitThreshold |
+   | handler commitThreshold usedMemory |
 
-   commitThreshold := 65.
-   handler := AlmostOutOfMemory addDefaultHandler: [ :ex | self halt ].
+   commitThreshold := 80.
+   handler := AlmostOutOfMemory addDefaultHandler: [ :ex | 
+     Transcript show: ('AlmostOutOfMemory: ', ex printString); cr.
+     System commitTransaction ifFalse: [Transcript show: 'AutoCommit failed'. nil error: 'AutoCommit failed' ].
+     System _vmMarkSweep. "GC executed to remove unnecessary objects"
+     Transcript show: ('GC Executed'); cr. 
+     usedMemory := System _tempObjSpacePercentUsedLastMark.
+     Transcript show: ('Used Memory: ', usedMemory printString); cr. 
+     (usedMemory < commitThreshold)
+     ifTrue: [System enableAlmostOutOfMemoryError]"we are cool the memory droped below threshold"
+     ifFalse: [(ObjectLogEntry trace: 'AlmostOutOfMemory' object: (System _vmInstanceCounts: 3)) addToLog.
+               System commitTransaction].
+     ex resume.
+   ].
    SessionTemps current at: #'AlmostOutOfMemoryStaticException' put: handler.
    System signalAlmostOutOfMemoryThreshold: commitThreshold.
 
-[BpmAppLinuxScripts startAllScript]
-  on: AlmostOutOfMemory enable
-  do: [:ex | ex error: ex description]. 
+  BpmAppLinuxScripts startAllScript. 
 %
 exit
 EOF
