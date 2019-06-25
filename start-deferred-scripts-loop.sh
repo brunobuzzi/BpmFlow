acf933b4ecdbe349b6b420d0129b6b8a6a5c82fc
bruno.buzzi <bruno.brasesco@gmail.com>
2019-06-25 09:52:26 -0300

    change 04 to fix issue #490

---
+++ start-deferred-scripts-loop.sh
@@ -0,0 +1,20 @@
+#Requires GS_HOME variable defined
+#! /bin/sh
+#set -x
+$GS_HOME/bin/startTopaz devKit_34 -il <<EOF >>MFC.out
+set user DataCurator password swordfish gemstone devKit_34
+login
+exec 
+   | handler commitThreshold |
+
+   commitThreshold := 65.
+   handler := AlmostOutOfMemory addDefaultHandler: [ :ex | self halt ].
+   SessionTemps current at: #'AlmostOutOfMemoryStaticException' put: handler.
+   System signalAlmostOutOfMemoryThreshold: commitThreshold.
+
+[BpmScriptExecutor default startUninterruptedLoop]
+  on: AlmostOutOfMemory enable
+  do: [:ex | ex error: ex description]. 
+%
+exit
+EOF
