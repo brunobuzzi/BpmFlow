31320e6c0d05dac2806cfa40f0e9ab826b090dda
bruno.buzzi <bruno.brasesco@gmail.com>
2019-10-28 12:05:55 -0300

    updates for #1006 and #1007

--- scripts/start-all.sh
+++ scripts/start-all.sh
@@ -1,6 +1,20 @@
 #Requires GS_HOME variable defined
 #! /bin/sh
 #set -x
+if [ "$1" == "-h" ]; then
+  echo "The command take no arugments";
+  echo "It will start all Web Servers contained in the file ports-all.ini:";
+  if [ ! -f ports-all.ini ]; 
+    then echo "ports-all.ini file does not exist command can not be executed"
+    else cat ports-all.ini;
+  fi
+  echo ;
+  exit 0
+fi
+if [ -z ${GS_HOME+x} ]; then
+  echo "GS_HOME variable is unset";
+  exit 0
+fi
 $GS_HOME/bin/startTopaz devKit_34 -u "WebServer" -il <<EOF >>MFC.out
 set user DataCurator password swordfish gemstone devKit_34
 login
