520f6b8a18cb383fb258bc50a8ccedfdab15184f
bruno.buzzi <bruno.brasesco@gmail.com>
2019-10-28 16:12:20 -0300

    update 03 for #1006 and #1007

--- scripts/start-on.sh
+++ scripts/start-on.sh
@@ -1,6 +1,17 @@
 #Requires GS_HOME variable defined
 #! /bin/sh
 #set -x
+if [ "$1" == "-h" ]; then
+  echo "Usage: start-on PORT"
+  echo "Start a Web Server on port number PORT"; 
+  echo "The environment variable GS_HOME must be set"; 
+  echo "This script is used in conjunction with stop-on script";   
+  exit 0
+fi
+if [ -z ${GS_HOME+x} ]; then
+  echo "GS_HOME variable is unset. Set this variable first and try again...";
+  exit 0
+fi
 $GS_HOME/bin/startTopaz devKit_34 -u "WebServer" -il <<EOF >>MFC.out
 set user DataCurator password swordfish gemstone devKit_34
 login
--- scripts/start-zn.sh
+++ scripts/start-zn.sh
@@ -1,6 +1,23 @@
 #Requires GS_HOME variable defined
 #! /bin/sh
 #set -x
+if [ "$1" == "-h" ]; then
+  echo "Usage: start-zn (with no arguments)"
+  echo "Start all Web Servers contained in the file (zn-ports.ini):"; 
+  if [ ! -f zn-ports.ini ]; 
+    then echo "zn-ports.ini file does not exist the script can not be executed"
+    else (cat ports-all.ini; echo)
+  fi
+  echo "The environment variable GS_HOME must be set";
+  echo "The (zn-ports.ini) file has to have the following format: port1,port2,port3";
+  echo "For example: 8787,8888,8989";  
+  echo "This script is used in conjunction with stop-zn script";   
+  exit 0
+fi
+if [ -z ${GS_HOME+x} ]; then
+  echo "GS_HOME variable is unset. Set this variable first and try again...";
+  exit 0
+fi
 $GS_HOME/bin/startTopaz devKit_34 -u "WebServer" -il <<EOF >>MFC.out
 set user DataCurator password swordfish gemstone devKit_34
 login
