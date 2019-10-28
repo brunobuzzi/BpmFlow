86a772e3a0ee4dbec0b7638a6bbedd6d3a533253
bruno.buzzi <bruno.brasesco@gmail.com>
2019-10-28 16:38:41 -0300

    update 04 for #1006 and #1007

--- scripts/register-application.sh
+++ scripts/register-application.sh
@@ -1,6 +1,18 @@
 #Requires GS_HOME variable defined
 #! /bin/sh
 #set -x
+if [ "$1" == "-h" ]; then
+  echo "Usage: register-application (with no arguments)"
+  echo "This script is to register Bpm Flow web application and it should be executed only once after the installation process";
+  echo "To unregister the web application see unregister-application script"     
+  echo "The environment variable GS_HOME must be set";
+  echo "This script is used in conjunction with unregister-application script";   
+  exit 0
+fi
+if [ -z ${GS_HOME+x} ]; then
+  echo "GS_HOME variable is unset. Set this variable first and try again...";
+  exit 0
+fi
 $GS_HOME/bin/startTopaz devKit_34 -il <<EOF >>MFC.out
 set user DataCurator password swordfish gemstone devKit_34
 login
--- scripts/unregister-application.sh
+++ scripts/unregister-application.sh
@@ -1,6 +1,18 @@
 #Requires GS_HOME variable defined
 #! /bin/sh
 #set -x
+if [ "$1" == "-h" ]; then
+  echo "Usage: unregister-application (with no arguments)"
+  echo "This script is to unregister Bpm Flow web application and if it is necessary it should be executed after registration";
+  echo "To register the web application again see register-application script"     
+  echo "The environment variable GS_HOME must be set";
+  echo "This script is used in conjunction with register-application script";   
+  exit 0
+fi
+if [ -z ${GS_HOME+x} ]; then
+  echo "GS_HOME variable is unset. Set this variable first and try again...";
+  exit 0
+fi
 $GS_HOME/bin/startTopaz devKit_34 -il <<EOF >>MFC.out
 set user DataCurator password swordfish gemstone devKit_34
 login
