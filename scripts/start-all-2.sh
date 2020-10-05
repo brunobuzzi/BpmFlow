2f53276a8dc2000021be50b271263e9043b77bdd
brunobuzzi <bruno.brasesco@gmail.com>
2020-10-05 14:34:39 -0400

    update 02 to fix issue #1156

---
+++ scripts/start-all-2.sh
@@ -0,0 +1,90 @@
+#! /bin/sh
+# Requires GS_HOME variable defined
+# This command will start a multi-thread Web Application Server where each port will be attended by a different (Gem) process. 
+# Following the previous example three Gem processes will be created to attend each port. 
+# The ports to be attended are defined in ports-all.ini. 
+# At registration time (register-application.sh) there is a list of ports in ports-all.ini and each will be active when this script is executed.
+
+PROGRAM_NAME="start_all"
+info() {
+  echo 1>&2
+  CURRENT_TIME=`date`
+  echo ${PROGRAM}[INFO]: $* [$CURRENT_TIME] 1>&2
+  echo 1>&2
+}
+error() {
+  echo 1>&2
+  echo [ERROR]: $* 
+  echo 1>&2
+}
+usage() {
+  error "Usage: ${PROGRAM_NAME} -s DBNAME -p ports"
+}
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+  echo "Usage: start-all -s STONE_NAME -p PORTS"
+  echo "Start all Web Servers contained in the file (ports-all.ini):"; 
+  if [ ! -f ports-all.ini ]; 
+    then echo "ports-all.ini file does not exist the script can not be executed"
+    else (cat ports-all.ini; echo)
+  fi
+  echo "The environment variable GS_HOME must be set";
+  echo "The (ports-all.ini) file has to have the following format: port1,port2,port3";
+  echo "For example: 8787,8888,8989";  
+  echo "This script is used in conjunction with stop-all script";   
+  exit 0
+fi
+if [ -z ${GS_HOME+x} ]; then
+  info "GS_HOME variable is unset. Set this variable first and try again...";
+  exit 0
+fi
+
+while getopts :l:s:p: opt; do
+  case $opt in
+    s) STONE=$OPTARG ;;
+    \?) error "Invalid option: -$OPTARG"
+      usage
+      exit 1
+      ;;
+    :)error "Option -$OPTARG requires Stone name and ports."
+      usage
+      exit 1
+     ;;
+  esac
+done
+
+if sh checkIfStoneExist.sh "$STONE"; 
+  then echo "" 
+  else 
+    info "Topaz for Stone named [$STONE] failed to start";
+    exit 0
+fi
+info "Start: Starting Gem processes as Web Servers"
+nohup $GS_HOME/bin/startTopaz $STONE -u "WebServer" -il <<EOF >>MFC.out &
+set user DataCurator password swordfish gemstone $STONE
+login
+exec 
+   | handler commitThreshold usedMemory |
+
+   commitThreshold := 80.
+   handler := AlmostOutOfMemory addDefaultHandler: [ :ex | 
+     Transcript show: ('AlmostOutOfMemory: ', ex printString); cr.
+     System commitTransaction ifFalse: [Transcript show: 'AutoCommit failed'. nil error: 'AutoCommit failed' ].
+     System _vmMarkSweep. "GC executed to remove unnecessary objects"
+     Transcript show: ('GC Executed'); cr. 
+     usedMemory := System _tempObjSpacePercentUsedLastMark.
+     Transcript show: ('Used Memory: ', usedMemory printString); cr. 
+     (usedMemory < commitThreshold)
+     ifTrue: [System enableAlmostOutOfMemoryError]"we are cool the memory droped below threshold"
+     ifFalse: [(ObjectLogEntry trace: 'AlmostOutOfMemory' object: (System _vmInstanceCounts: 3)) addToLog.
+               System commitTransaction].
+     ex resume.
+   ].
+   SessionTemps current at: #'AlmostOutOfMemoryStaticException' put: handler.
+   System signalAlmostOutOfMemoryThreshold: commitThreshold.
+
+  BpmAppLinuxScripts startAllScript. 
+%
+exit
+EOF
+info "Finish: Starting Gem processes as Web Servers"
